ce03289feade53705167fb88f747a999daa2f496
Kosta Harlan <kostajh@gmail.com>
2014-06-15 21:26:52 -0400

    Adjust CSS for front page post listings

--- drupalroot/sites/all/themes/dbc/templates/page.tpl.php
+++ drupalroot/sites/all/themes/dbc/templates/page.tpl.php
@@ -100,13 +100,13 @@
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-<!--
+
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1 class="title" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
--->
+
       <?php print $messages; ?>
       <?php if ($tabs = render($tabs)): ?>
         <div class="tabs"><?php print $tabs; ?></div>
